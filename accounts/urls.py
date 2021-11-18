4f997a651fb0e66dec922c9c3f146963435a6549
tshopgroupproject <93291584+tshopgroupproject@users.noreply.github.com>
2021-11-17 20:31:12 -0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
-    path('logout/', logout, name='logout'),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
     url(r'^reset/$', reset, name='reset'),
     path('', sendMail),

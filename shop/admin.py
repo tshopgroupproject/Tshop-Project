ec2b26a805dba48e81caeca598281dc7ca9b8da5
tshopgroupproject <93291584+tshopgroupproject@users.noreply.github.com>
2021-12-13 19:32:32 -0600

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Review
 
 
 @admin.register(Category)
@@ -15,3 +15,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['name', 'review']
+    prepopulated_fields = {'review': ('name',)}
